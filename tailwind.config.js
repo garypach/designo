02c11c46cb8a3ad1e774b5fd50a0a241002c6919
Gary <58466698+garypach@users.noreply.github.com>
2022-01-08 17:40:22 -0800

    added content to all pages except contact
    mobile first design

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "peach": "#E7816B",
+        "light-Peach": "#FFAD9B",
+        "black":"#1D1C1E",
+        "dark-grey":"#333136",
+        "light-grey":"#F1F3F5"
+
+      },
+    },
+  },
+  plugins: [],
+};
