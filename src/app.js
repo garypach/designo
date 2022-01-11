c6843bde64844a12c432ae3b6711145bfcedfb4a
Gary <58466698+garypach@users.noreply.github.com>
2022-01-11 13:30:48 -0800

    contact page responsive design

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+window.onload = function(){
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
@@ -15,5 +16,65 @@ navLink.forEach(n => n.addEventListener("click", closeMenu));
 function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
+}
+
+let submitButton = document.getElementsByClassName('form-button')[0];
+let nameInput = document.getElementsByClassName('name-input')[0];
+let phoneInput = document.getElementsByClassName('phone-input')[0];
+let messageInput = document.getElementsByClassName('message-input')[0];
+
+let emailInput = document.getElementsByClassName('email-input')[0];
+
+
+let nameremoveErr = ()=>{
+    document.getElementsByClassName("name-error-message")[0].classList.remove("name-error-message--active")
+}
+
+let emailremoveErr = ()=>{
+    document.getElementsByClassName("email-error-message")[0].classList.remove("email-error-message--active")
+}
+let emailaddErr = ()=>{
+    document.getElementsByClassName("email-error-message")[0].classList.add("email-error-message--active")
+}
+
+let phoneremoveErr = ()=>{
+    document.getElementsByClassName("phone-error-message")[0].classList.remove("phone-error-message--active")
+}
+let messageremoveErr = ()=>{
+    document.getElementsByClassName("message-error-message")[0].classList.remove("message-error-message--active")
+}
+
+submitButton.addEventListener('click', ()=>{
+    if(document.getElementsByClassName('name-input')[0].value.length == 0){
+        document.getElementsByClassName("name-error-message")[0].classList.add("name-error-message--active");
+    }
+    if(document.getElementsByClassName('email-input')[0].value.length == 0){
+        document.getElementsByClassName("email-error-message")[0].classList.add("email-error-message--active");
+    }
+
+    if(document.getElementsByClassName('phone-input')[0].value.length == 0){
+        document.getElementsByClassName("phone-error-message")[0].classList.add("phone-error-message--active");
+    }
+
+    if(document.getElementsByClassName('message-input')[0].value.length == 0){
+        document.getElementsByClassName("message-error-message")[0].classList.add("message-error-message--active");
+    }
+})
+
+nameInput.addEventListener('click', ()=>{
+    nameremoveErr();
+})
+emailInput.addEventListener('click', ()=>{
+    emailremoveErr();
+})
+
+phoneInput.addEventListener('click', ()=>{
+    phoneremoveErr();
+})
+
+messageInput.addEventListener('click', ()=>{
+    messageremoveErr();
+})
+
 }
 
